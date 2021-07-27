5f9f94557e0ea6b6b52dbddf76d875e56ee1e44e
Jhones Digno <jhonesdigno777@gmail.com>
2021-07-27 18:12:30 +0800

    Remaked it

--- js/reveal.js
+++ js/reveal.js
@@ -25,9 +25,8 @@ window.onscroll = function () {
     for (var i = 0; i < reveals.length; i++) {
       var windowheight = window.innerHeight;
       var revealtop = reveals[i].getBoundingClientRect().top;
-      var revealpoint = 150;
 
-      if (revealtop < windowheight - revealtop) {
+      if (revealtop - 300 < windowheight - revealtop) {
         reveals[i].classList.add("active");
       } else {
         reveals[i].classList.remove("active");
